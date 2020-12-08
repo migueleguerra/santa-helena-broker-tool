477c35da89b6524283d701449003025ff1b7e5dd
migueleguerra <guetop88@gmail.com>
2020-12-08 01:57:04 -0600

    quote tool first part

---
+++ js/actions.js
@@ -0,0 +1,29 @@
+const SH_PASSWORD = "carza2020";
+
+function cleanForm() {
+  financialProjection.resetValues();
+
+  let slotSelector = getField("lotes");
+  slotSelector.value = JSON.stringify(
+    lotes[slotSelector.options.selectedIndex]
+  );
+
+  slotSelector.dispatchEvent(new Event("change"));
+}
+
+function enter() {
+  const password = getField("password-enter").value;
+
+  if (password !== undefined && password != "" && password === SH_PASSWORD) {
+    setDate();
+    getField("broker").textContent = getField("broker-enter").value;
+    getField("client").textContent = getField("client-enter").value;
+    getField("email").textContent = getField("email-enter").value;
+    getField("tel").textContent = getField("tel-enter").value;
+
+    getField("popup").style = "visibility: hidden";
+    return;
+  }
+
+  showErrorMsg("La contraseña es incorrecta.");
+}
